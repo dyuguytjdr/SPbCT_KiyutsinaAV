827f2c4f30dca8dbe203100b838c53a9d6ef2fad
dyuguytjdr <74043933+dyuguytjdr@users.noreply.github.com>
2020-11-06 11:44:08 +0300

    Add files via upload

---
+++ Nafty.cpp
@@ -0,0 +1,40 @@
+﻿#include <process.h>
+#include <stdarg.h>
+#include <string.h>
+#include <iostream>
+#include "windows.h"
+
+
+using namespace std;
+
+void fork(int i) {
+    cout << "Создание процесса № " << i << "\n";
+}
+void execl(int i, int a) {
+    cout << "Передача числа " << a << " в процесс " << i << endl;
+}
+void waitpid(int i) {
+    cout << "Ожидание процесса № " << i << "\n";
+    Sleep(200);
+}
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "ru");
+    char* pEnd;
+    cout << "Начало программы\n\n";
+    int proc = strtol(argv[1], &pEnd, 10);
+    int num = strtol(argv[2], &pEnd, 10);
+    for (int i = 1; i <= proc; i++) {
+        fork(i);
+    }
+    cout << "\nПроцессы созданы\n\n";
+    for (int i = 1; i <= proc; i++) {
+        execl(i, num);
+    }
+    cout << "\nПередача чисел завершина\n\n";
+    for (int i = 1; i <= proc; i++) {
+        waitpid(i);
+    }
+    cout << "\nКонец программы\n";
+}
